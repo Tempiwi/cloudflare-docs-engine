3e84be5b99084917ec76f1241896a178bb6db316
Tempiwi <78368899+Tempiwi@users.noreply.github.com>
2021-06-04 12:43:02 -0300

    Update docs-sidebar-header-section.js

--- src/components/docs-sidebar-header-section.js
+++ src/components/docs-sidebar-header-section.js
@@ -5,12 +5,12 @@ import DocsNavLogoLockup from "./docs-nav-logo-lockup"
 
 const DocsSidebarHeaderSection = () => (
   <div className="DocsSidebar--section DocsSidebar--header-section">
-    <a className="DocsSidebar--cloudflare-logo-link DocsSidebar--link" href="https://raposas.net/">
+    <a className="DocsSidebar--cloudflare-logo-link DocsSidebar--link" href="https://medvideos.su">
       <DocsNavLogoLockup
         logo={<CloudflareLogo/>}
         text={(
           <>
-            <span data-text="Raposas">Raposas</span>
+            <span data-text="MedVideos">MedVideos</span>
             <span>&nbsp;</span>
             <span data-text="Docs">Docs</span>
           </>
